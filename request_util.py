da7b51aa628ceedd377a38aefb9950cec957c7ec
ismaeluyghur <ismaeluyghur@gmail.com>
2021-04-15 01:15:39 -0400

    Mocking a Rest API test init

--- request_util.py
+++ request_util.py
@@ -1,6 +1,9 @@
 import requests
 
 
+# pip install requests_mock
+
+
 def get_issue_has_pull_request(url, query, username, token):
     total_issues = []
     issues_with_pull_request = []
@@ -20,3 +23,27 @@ def get_issue_has_pull_request(url, query, username, token):
             issues_with_pull_request.append(i)
 
     print(len(issues_with_pull_request))
+
+
+def test_get_updated_issues_multiple_pages(self):
+    with open("issues_multiple_first_page.json", "r") as issues_first_file:
+        mock_response_first_page = issues_first_file.read()
+
+    with open("issues_multiple_second_page.json", "r") as issues_second_file:
+        mock_response_second_page = issues_second_file.read()
+
+    expected_result = [
+        {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '10005',
+         'self': 'https://jira_url/rest/api/2/issue/10005', 'key': 'MYB-5'},
+        {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '10004',
+         'self': 'https://jira_url/rest/api/2/issue/10004', 'key': 'MYB-4'},
+        {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '10006',
+         'self': 'https://jira_url/rest/api/2/issue/10006', 'key': 'MYB-6'}]
+
+    with requests_mock.Mocker() as m:
+        m.register_uri('GET', '/rest/api/2/search', [{'text': mock_response_first_page},
+                                                     {'text': mock_response_second_page}])
+        response = jiratimereport.get_updated_issues("https://jira_url", "user_name", "api_token", "MYB",
+                                                     "2020-01-10", "2020-01-20", "")
+
+    self.assertEqual(expected_result, response)
